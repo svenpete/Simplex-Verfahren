65a7968d278eb1747ea5f17567b3ce4930623e90
svenpetersen <sven.petersen911@gmail.com>
2020-03-26 15:32:26 +0100

    Initial commit

---
+++ src/Gui/Controller.java
@@ -0,0 +1,42 @@
+package Gui;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+
+public class Controller {
+
+    @FXML
+    private TextField NB;
+
+    @FXML
+    private TextField Variablen;
+
+    @FXML
+    private Button sRow;
+
+    @FXML
+    private Button sColumn;
+
+    @FXML
+    private TableView<?> table;
+
+    @FXML
+    void changeColumn(ActionEvent event)
+    {
+        checkInput(Variablen.getCharacters());
+        TableColumn firstColumn = new TableColumn("x1");
+        TableColumn secondColumn = new TableColumn("x2");
+        table.getColumns().addAll(firstColumn, secondColumn);
+    }
+    private static int checkInput(CharSequence input)
+    {
+        Integer number = Integer.parseInt(input,0,input.length(),10);
+        return number;
+    }
+
+
+}
---
+++ src/Gui/Main.java
@@ -0,0 +1,29 @@
+package Gui;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application
+{
+    public static void main(String [] args)
+    {
+     launch(args);
+     System.out.println();
+
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception
+    {
+        stage.setTitle("Simplex");
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("simplex.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();  // Stage alles drumherum
+
+    }
+}
