2e2c8f2ed26db92c862a2bf48de498fa0316f2e9
Constantin Brans <constantin.brans@hs-osnabrueck.de>
2020-04-01 19:18:42 +0200

    Mehrdimensionales Array für die Tabelle

--- src/Core/Main.java
+++ src/Core/Main.java
@@ -1,4 +1,11 @@
 package Core;
 
-public class Main {
+
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Table.createTable();
+    }
 }
--- src/Core/Table.java
+++ src/Core/Table.java
@@ -1,30 +1,44 @@
 package Core;
 
+import java.util.Scanner;
+
 public class Table
 {
     private int columns;
     private int rows;
-    private int[][] table;
 
-    public Table(int rows, int columns)
+
+    private Table(int rows,int columns)
     {
         this.columns = columns;
         this.rows = rows;
-        createTable(rows,columns);
+
     }
 
-
-    private int[][] createTable(int rows,int columns)
+    private int[][] createTableArray(int rows,int columns)
     {
-         table = new int[rows-1][columns-1];
-        System.out.println("Die Tabelle besteht aus "+rows+ " Zeilen und "+ columns+ " Spalten");
-
+        int[][] table;
+        table = new int[rows-1][columns-1];
         return table;
     }
 
-
-    public static void main(String[] args)
+    public static void createTable()
     {
-        Table table = new Table(3,2);
+        Scanner NBScanner = new Scanner(System.in);
+        Scanner VBScanner = new Scanner(System.in);
+
+        System.out.println("Geben Sie die Anzahl an Nebenbedingugnen an:");
+        int constraint = NBScanner.nextInt();
+
+
+        System.out.println("Geben Sie die Anzahl an Variabeln an:");
+        int variabels = VBScanner.nextInt();
+
+        Table table = new Table(constraint,variabels);
+        int[][] table1 = table.createTableArray(constraint,variabels);
+        System.out.println("Es gibt " +(table1.length+1)+" Nebenbedingungen. Es gibt " + (table1[0].length+1) +" Variabeln.");
     }
+
+
+
 }
