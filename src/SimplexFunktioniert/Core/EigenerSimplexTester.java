e4b9498cc234ff4feb2dfcad3ba1cc6709fced1b
conbrans <constantin.brans@hs-osnabrueck.de>
2020-06-03 14:14:03 +0200

    Aufräumen und Englisch

--- src/SimplexFunktioniert/Core/EigenerSimplexTester.java
+++ src/SimplexFunktioniert/Core/EigenerSimplexTester.java
@@ -8,13 +8,7 @@ public class EigenerSimplexTester {
         boolean quit = false;
         int i=0;
         EigenerSimplex e = new EigenerSimplex(zeile,spalte);
-        float[][] standardized =  {
-                {1,1,1,1,0,0,4},
-                {1,3,1,0,1,0,6},
-                {1,3,4,0,0,1,10},
-                {-3,-5,-4,0,0,0,0},
 
-        };
 
         System.out.println("Dies ist die Ausgangsmatrix");
         System.out.println("-------------------------------------");
@@ -25,7 +19,7 @@ public class EigenerSimplexTester {
             i++;
 
 
-           Error error = e.berechneSimplex();
+           Error error = e.calcSimplex();
             System.out.println("-------------------------------------");
 
            if (error == Error.STRING_IS_OPTIMAL) {
